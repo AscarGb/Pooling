7a429ee19392a6861fd5e479bc5d92d744ec874e
AscarGb <ascargb>
2019-05-20 23:45:09 +0500

    Добавьте файлы проекта.

---
+++ Pooling.AppConsole/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pooling.AppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("press to start");
+            Console.ReadLine();
+
+            var bigListPool = new Pool<List<long>>(Creator, Clearer);
+
+            Task.Run(async () =>
+            {
+                List<Task> tl = new List<Task>();
+
+                for (int i = 0; i < 1000; i++)
+                {
+                    var task = Task.Run(() =>
+                    {
+
+                        List<long> list = null;
+
+                        try
+                        {
+                            list = bigListPool.Rent();
+                        }
+                        finally
+                        {
+                            if (list != null)
+                                bigListPool.Return(list);
+                        }
+
+                    });
+
+                    tl.Add(task);
+                }
+
+                await Task.WhenAll(tl);
+
+                Console.WriteLine(bigListPool.Count());
+            });
+
+            Console.ReadLine();
+        }
+
+        static List<long> Creator() => new List<long>(1024 * 1024);
+
+
+        static void Clearer(List<long> l) => l.ForEach(i => i = 0);
+    }
+
+}
---
+++ Pooling/Pool.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Pooling
+{
+    public sealed class Pool<T>
+    {
+        private ConcurrentBag<T> _items =
+            new ConcurrentBag<T>();
+
+        private readonly Func<T> _itemCreator = null;
+        private readonly Action<T> _itemClearer = null;
+
+        public Pool(Func<T> itemCreator)
+        {
+            _itemCreator = itemCreator ?? throw new ArgumentNullException("itemCreator");
+        }
+
+        public Pool(Func<T> itemCreator, Action<T> itemClearer) : this(itemCreator)
+        {
+            _itemClearer = itemClearer ?? throw new ArgumentNullException("itemClearer");
+        }
+
+        public T Rent()
+        {
+            if (_items.TryTake(out var item))
+                return item;
+
+            return _itemCreator();
+        }
+
+        public void Return(T item)
+        {
+            _itemClearer?.Invoke(item);
+
+            _items.Add(item);
+        }
+
+        public int Count()
+        {
+            return _items.Count;
+        }
+    }
+}
